475163377d62e5e0775b6a72636c47c8c1461727
Don'tReadMe <rockkulkarni37@gmail.com>
2025-09-24 10:55:12 +0530

    Time: 93 ms (89.45%), Space: 51.9 MB (44.93%) - LeetHub

---
+++ 2429-design-a-food-rating-system/2429-design-a-food-rating-system.py
@@ -0,0 +1,36 @@
+class FoodRatings:
+
+    def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
+        self.food_to_cuisine = {}
+        self.food_to_rating = {}
+        self.cuisine_heaps = {}
+
+        for f, c, r in zip(foods, cuisines, ratings):
+            self.food_to_cuisine[f] = c
+            self.food_to_rating[f] = r
+            if c not in self.cuisine_heaps:
+                self.cuisine_heaps[c] = []
+            heapq.heappush(self.cuisine_heaps[c], (-r, f))
+
+
+    def changeRating(self, food: str, newRating: int) -> None:
+        cuisine = self.food_to_cuisine[food]
+        self.food_to_rating[food] = newRating
+        heapq.heappush(self.cuisine_heaps[cuisine], (-newRating, food))
+        
+
+    def highestRated(self, cuisine: str) -> str:
+        heap = self.cuisine_heaps[cuisine]
+        while heap:
+            rating, food = heap[0]
+            if -rating == self.food_to_rating[food]:
+                return food
+            heapq.heappop(heap)
+        return ""
+        
+
+
+# Your FoodRatings object will be instantiated and called as such:
+# obj = FoodRatings(foods, cuisines, ratings)
+# obj.changeRating(food,newRating)
+# param_2 = obj.highestRated(cuisine)
