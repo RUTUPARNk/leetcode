0615e318ed444762a35505f7ba049eae43e0b4af
Don'tReadMe <rockkulkarni37@gmail.com>
2025-09-26 06:50:33 +0530

    Time: 426 ms (57.71%), Space: 18 MB (34.35%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.py
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -5,6 +5,15 @@ class Solution:
         n = len(nums)
         # step 2: loop from the end for the largest side c
         for i in range(n - 1, 1, -1):
+            # check 1: the worst-case shortcut
+            # if the two largest remaining sides can't form a triangle, then no other sides can either. we can skip the rest of the inner loop
+            if nums[i - 2] + nums[i-1] <= nums[i]:
+                continue
+            # The best-case shortcut, if the two smallest numbers can form a triangle with nums[i], then every pair in this range will. we can calculate the total and stop the loop early.
+            if nums[0] + nums[1] > nums[i]:
+                # this part is bit trick and needs a more complex formula, but the idea is to calculate all combinations from 0 to i. the core algorithm handles this more simply without this check.
+                pass
+                
             j = 0 # pointer for the smallest side a
             k = i - 1 # pointer for the second side b
             # step 3: two pointer loop
